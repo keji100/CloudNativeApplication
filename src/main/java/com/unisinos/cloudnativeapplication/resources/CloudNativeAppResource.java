b1b0b10a4b897e94f3621a80841576c7039fc651
keji100 <willianjapa@gmail.com>
2021-04-04 19:17:41 -0300

    Ajuste para pom.xml e resource para pagina principal

--- src/main/java/com/unisinos/cloudnativeapplication/resources/CloudNativeAppResource.java
+++ src/main/java/com/unisinos/cloudnativeapplication/resources/CloudNativeAppResource.java
@@ -4,16 +4,14 @@ import java.io.Serializable;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/cloud")
 public class CloudNativeAppResource implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@GetMapping(value = "/materia")
+	@GetMapping("/")
 	public ResponseEntity<String> getConfigs() {
 		return ResponseEntity.ok("2021/1 - Arquitetura de Software - GR16032-00008 - Kleinner Silva Farias de Oliveira");
 	}
