43b9a2e07bb48b8ca0ed9c7c91d37d68f80379f9
keji100 <36178537+keji100@users.noreply.github.com>
2021-04-08 18:51:23 -0300

    Rollback sem a chamada de alunos

--- src/main/java/com/unisinos/cloudnativeapplication/resources/CloudNativeAppResource.java
+++ src/main/java/com/unisinos/cloudnativeapplication/resources/CloudNativeAppResource.java
@@ -21,9 +21,4 @@ public class CloudNativeAppResource implements Serializable {
 		return ResponseEntity.ok("Kleinner Silva Farias de Oliveira");
 	}
 	
-	@GetMapping("/alunos")
-	public ResponseEntity<String> getAlunos() {
-		return ResponseEntity.ok("Willian, Renato, Dione e Augusto");
-	}
-	
 }
